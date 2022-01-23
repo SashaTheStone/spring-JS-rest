28d8786d8765ac97e4f0e8672981f2be5445d2e2
SashaTheStone <ApTolstik@mail.ru>
2022-01-23 05:50:05 +0300

    Что то сделано

---
+++ src/main/java/crud/controllers/AdminController.java
@@ -0,0 +1,72 @@
+package crud.controllers;
+
+import crud.models.User;
+import crud.services.RoleService;
+import crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public String userList(Principal principal, Model model){
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("user", userService.getUserByEmail(principal.getName()));
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "views/userPage/usersList";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("newUser") User newUser,
+                          Principal principal, Model model){
+        model.addAttribute("user", userService.getUserByEmail(principal.getName()));
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "views/userPage/userCreate";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("newUser") User newUser,
+                         @RequestParam("userRolesID") Long[] roleList, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "views/userPage/userCreate";
+        }
+        newUser.setRoles(Arrays.stream(roleList).map(roleService::readRoleByID).collect(Collectors.toSet()));
+        userService.saveUser(newUser);
+        return "redirect:/admin/new";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("newUser") User user, @RequestParam("userRolesID") Long[] roleList,
+                             BindingResult bindingResult, @PathVariable("id") long id) {
+        if (bindingResult.hasErrors()) {
+            return "views/userPage/userUpdate";
+        }
+        user.setRoles(Arrays.stream(roleList).map(roleService::readRoleByID).collect(Collectors.toSet()));
+        userService.updateUser(id,user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+}
--- src/main/java/crud/controllers/UserController.java
+++ src/main/java/crud/controllers/UserController.java
@@ -1,78 +1,27 @@
 package crud.controllers;
 
-import crud.models.User;
 import crud.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.security.Principal;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/admin")
-    public String userList(Model model){
-        model.addAttribute("users", userService.getAllUsers());
-        return "views/userPage/usersList";
-    }
-
-    @GetMapping("/user")
+    @GetMapping()
     public String currentUser(Principal principal, Model model){
         model.addAttribute("user", userService.getUserByEmail(principal.getName()));
         return "views/userPage/userInfo";
-    }
-
-    @GetMapping("/admin/{id}")
-    public String UserInfo(@PathVariable("id") long id, Model model){
-        model.addAttribute("user", userService.readUserByID(id));
-        return "views/userPage/userInfoAdmin";
-    }
-
-    @GetMapping("/admin/new")
-    public String newUser(@ModelAttribute("user") User user){
-        return "views/userPage/userCreate";
-    }
-
-    @PostMapping("/admin")
-    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            return "views/userPage/userCreate";
-        }
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/admin/{id}/edit")
-    public String editUser(Model model, @PathVariable("id") long id){
-        model.addAttribute("user", userService.readUserByID(id));
-        return "views/userPage/userUpdate";
-    }
-
-    @PatchMapping("/admin/{id}")
-    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
-                             @PathVariable("id") long id) {
-        if (bindingResult.hasErrors()) {
-            return "views/userPage/userUpdate";
-        }
-        userService.updateUser(id,user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("admin/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
     }
 
 }
